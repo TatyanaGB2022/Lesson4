13aae1a1fd0e1fa6796fb3fdb2cf6957e85ff5d0
Tatyana <tatyana.kurii@mail.ru>
2023-02-01 21:32:32 +0600

    Mission 4

---
+++ Mission4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и
+// единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int Prompt(string message)
+{
+  System.Console.Write($"{message}: ");
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+int[] CreatArray(int X)
+{
+  int[] array = new int[X];
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(0, 2);
+  }
+  return array;
+}
+void ShowArray(int[] array)
+{
+  for (int j = 0; j < array.Length; j++)
+  {
+    Console.Write($"{array[j]}");
+  }
+}
+
+int X = Prompt("Size of array ");
+int[] array = CreatArray(X);
+ShowArray(array);
