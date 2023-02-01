90407768572822b4fc50697bf3b6195108a3de6c
Tatyana <tatyana.kurii@mail.ru>
2023-02-01 20:55:45 +0600

    Mission 3

---
+++ Mission3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24
+//5 -> 120
+
+int Prompt(string message)
+{
+  System.Console.Write($"{message} > ");
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int Factorial(int n)
+{
+  int prod = 1;
+  for (int i = 1; i <= n; i++)
+  {
+    prod = prod * i;
+  }
+  return prod;
+}
+int number = Prompt("Введите число");
+
+int fakt = Factorial(number);
+System.Console.WriteLine($"Произведение (факториал) числа {number} = {fakt}");
